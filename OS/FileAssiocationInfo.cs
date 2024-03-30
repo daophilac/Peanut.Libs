f3d31aadf70d6e9abd009a9fb39cf04997f99a0a
daophilac <daophilac@hotmail.com>
2024-03-31 04:42:49 +0700

    Add documentation

--- OS/FileAssiocationInfo.cs
+++ OS/FileAssiocationInfo.cs
@@ -4,13 +4,46 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
 namespace Peanut.Libs.OS {
+    /// <summary>
+    /// Provides functionalities to deal with associated icons.
+    /// </summary>
     public partial class FileAssiocationInfo {
-        public string? ApplicationName { get; private set; }
-        public string? ApplicationPath { get; private set; }
-        public Icon[]? IconsLarge { get; private set; }
-        public Icon[]? IconsSmall { get; private set; }
-        public int IconIndex { get; private set; }
+#nullable disable
+        /// <summary>
+        /// Gets the ProgId.
+        /// </summary>
+        public string ProgId { get; private set; }
 
+        /// <summary>
+        /// Gets the application name.
+        /// </summary>
+        public string ApplicationName { get; private set; }
+#nullable enable
+
+        /// <summary>
+        /// Gets the executable path of the application.
+        /// </summary>
+        public string? ApplicationPath { get; private set; }
+
+        /// <summary>
+        /// Gets the list of found large icons.
+        /// </summary>
+        public Icon[]? IconsLarge { get; private set; }
+
+        /// <summary>
+        /// Gets the list of found small icons.
+        /// </summary>
+        public Icon[]? IconsSmall { get; private set; }
+
+        /// <summary>
+        /// Gets the icon large index which is used to display to program icon.
+        /// </summary>
+        public int IconIndex { get; private set; } = -1;
+
+        /// <summary>
+        /// Creates a bitmap using the found large icons and the <see cref="IconIndex"/>.
+        /// </summary>
+        /// <returns>The created bitmap.</returns>
         public Bitmap? CreateIconImage() {
             if (IconsLarge == null) {
                 return null;
@@ -29,6 +62,11 @@ namespace Peanut.Libs.OS {
         [LibraryImport("user32.dll")]
         private static partial int DestroyIcon(IntPtr hIcon);
 
+        /// <summary>
+        /// Gets a list of <see cref="FileAssiocationInfo"/> items with respect to a specific file extension.
+        /// </summary>
+        /// <param name="extension">The extension to be found.</param>
+        /// <returns>The list of found items.</returns>
         public static List<FileAssiocationInfo>? GetAssociatedPrograms(string extension) {
             RegistryKey? extensionKey = Registry.ClassesRoot.OpenSubKey($".{extension.ToLower()}\\OpenWithProgIDs");
             if (extensionKey == null) {
@@ -40,12 +78,25 @@ namespace Peanut.Libs.OS {
                 return null;
             }
 
-            List<FileAssiocationInfo> result = new List<FileAssiocationInfo>();
+            List<FileAssiocationInfo> result = new();
             foreach (string progId in progIds) {
                 RegistryKey? progKey = Registry.ClassesRoot.OpenSubKey($"{progId}\\Application");
                 if (progKey != null) {
                     FileAssiocationInfo info = Construct(progKey);
+                    info.ProgId = progId;
                     result.Add(info);
+                }
+                else {
+                    progKey = Registry.ClassesRoot.OpenSubKey($"{progId}\\DefaultIcon");
+                    if (progKey != null) {
+                        FileAssiocationInfo info = Construct(progKey);
+                        if (info.ApplicationPath != null
+                            && info.ApplicationPath.ToLower().EndsWith(".exe")) {
+                            info.ApplicationName = progId;
+                            info.ProgId = progId;
+                            result.Add(info);
+                        }
+                    }
                 }
             }
             return result;
@@ -53,7 +104,7 @@ namespace Peanut.Libs.OS {
 
         private static FileAssiocationInfo Construct(RegistryKey key) {
             string[] valueNames = key.GetValueNames();
-            FileAssiocationInfo info = new FileAssiocationInfo();
+            FileAssiocationInfo info = new();
             foreach (string name in valueNames) {
                 ProcessValue(info, key, name);
             }
@@ -69,12 +120,14 @@ namespace Peanut.Libs.OS {
                 case "ApplicationName":
                     info.ApplicationName = value;
                     break;
+                case "":
                 case "ApplicationIcon":
-                    string[] parts = value.Split(',');
+                    string[] parts = value.Split(',').Select(x => x.Trim('"')).ToArray();
+                    string path = parts[0];
                     if (File.Exists(parts[0])) {
                         info.ApplicationPath = parts[0];
                         if (parts.Length >= 2 && int.TryParse(parts[1], out int index)) {
-                            info.IconIndex = index;
+                            info.IconIndex = Math.Max(0, index);
                         }
                         else {
                             info.IconIndex = 0;
@@ -106,7 +159,6 @@ namespace Peanut.Libs.OS {
 
                 Icon[] iconsLarge = new Icon[iconsCount];
                 Icon[] iconsSmall = new Icon[iconsCount];
-
                 for (int x = 0; x < iconsCount; x++) {
                     if (pIconsLarge != null) {
                         iconsLarge[x] = (Icon)Icon.FromHandle(pIconsLarge[x]).Clone();
